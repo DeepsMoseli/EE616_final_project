a53e5241c11e393a0c42fedebacf60ad86566de5
Moseli Motsoehli <u12061019@tuks.co.za>
2020-04-21 11:49:14 -1000

    init implementation

---
+++ src/data_gen.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 11:42:18 2020
+
+@author: Deeps
+"""
+
+########################dataset creation##################
+
+import torch
+from torch.utils import data
+
+class Dataset(data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, list_IDs, labels):
+        'Initialization'
+        self.labels = labels
+        self.list_IDs = list_IDs
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.list_IDs)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        ID = self.list_IDs[index]
+
+        # Load data and get label
+        X = torch.load('Dataset/' + ID + '.jpg')
+        y = self.labels[ID]
+
+        return X, y
