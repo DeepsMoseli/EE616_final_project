f120fd205b7150224f6a707ae90e00507210dbb8
Moseli Motsoehli <u12061019@tuks.co.za>
2020-04-28 23:10:54 -1000

    nonesquare with data augmentation
    data aug, non square images

---
+++ src/augment.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 12:25:28 2020
+
+@author: Deeps
+"""
+
+from PIL import Image
+from torchvision import transforms
+import pandas as pd
+
+class Augmentations:
+    def __init__(self,angles =[45,90,150,180,270]):
+        self.angles = angles
+        
+    def rotate_append(self,number,label):
+        hold = []
+        labels = [label for k in self.angles]
+        img = Image.open('Dataset/' + str(number) + '.jpg')
+        
+        for k in range(len(self.angles)):
+            imageName = 120+(len(self.angles)*(number-1))+(k+1)
+            hold.append(imageName)
+            img_rot = img.rotate(self.angles[k])
+            img_rot.save('Dataset/' + str(imageName) + '.jpg')
+        return pd.DataFrame({"image":hold,"label":labels})
+
+
+
--- src/myimplementation.py
+++ src/myimplementation.py
@@ -20,18 +20,31 @@ import torch.nn.functional as F
 from torchvision import datasets,models, transforms
 from torch.utils.data import Dataset, Subset, DataLoader, random_split
 from data_gen import Dataset
+from augment import Augmentations as aug
 
 # Load the dataset and train, val, test splits
 
-num_epochs = 200
+num_epochs = 5
 num_classes = 2
-batch_size = 3
-learning_rate = 0.0005
+batch_size = 8
+learning_rate = 0.001
 datacsv = pd.read_csv("data.csv")
+
 cv_results = []
+
+#######################Data Augmentation##################################
+data_base_aug = dict(datacsv)
+data_base_aug["image"] = list(data_base_aug["image"])
+data_base_aug["label"] = list(data_base_aug["label"])
+
+rot = aug()
+for k in range(len(data_base_aug["image"])):
+    result = rot.rotate_append(data_base_aug["image"][k],data_base_aug["label"][k])
+    datacsv = datacsv.append(result,ignore_index = True)
 ##################Create dataset generator##################################
 
-for cv in range(6):
+
+for cv in range(1):
     x_train,x_test =tts(datacsv["image"],test_size=1/6, shuffle=True)
     partition = {'train':list(x_train),'validation':list(x_test)}
     labels = {}
@@ -61,30 +74,35 @@ for cv in range(6):
             # If you have many layers, consider using nn.Sequential() to simplify your code
             # self.fc1 = nn.Linear(28*28, 8) # from 28x28 input image to hidden layer of size 256
             # self.fc2 = nn.Linear(8,10) # from hidden layer to 10 class scores
-            self.conv1 = nn.Conv2d(3, 16, 5) #18*128*128
-            self.pool = nn.MaxPool2d(2, 2) #18*64*64
-            self.conv2 = nn.Conv2d(16, 32, 5) #36*64*64
-            self.fc1 = nn.Linear(32 * 29 * 29, 128)
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.conv2 = nn.Conv2d(6, 12, 5)
+            self.conv3 = nn.Conv2d(12, 6, (5,4))
+            self.conv4 = nn.Conv2d(6, 3, (7,4))
+            self.pool = nn.MaxPool2d(2, 2) 
+            self.poollast = nn.MaxPool2d(3, 2) 
+            self.fc1 = nn.Linear(3 * 42 * 29, 128)
             self.fc2 = nn.Linear(128, 64)
             self.fc3 = nn.Linear(64, num_classes)
             
     
         def forward(self,x):
             # TODO: Design your own network, implement forward pass here
-            x = self.pool(F.relu(self.conv1(x))) #16*124*124 -> 16*62*62
-            x = self.pool(F.relu(self.conv2(x))) #32*58*58 -> 32*29*29
-            x = x.view(-1, 32 * 29 * 29)
+            x = self.pool(F.relu(self.conv1(x))) #3*748*512 -> 6*744*508 -> 6*372*254
+            x = self.pool(F.relu(self.conv2(x))) #6*372*254 -> 12*368*250 -> 12*184*125
+            x = self.pool(F.relu(self.conv3(x))) #12*184*125 -> 6*180*122 -> 6*90*61
+            x = self.pool(F.relu(self.conv4(x))) #6*90*61-> 3*84*58 -> 3*42*29
+            x = x.view(-1, 3 * 42 * 29)
             x = F.dropout(F.relu(self.fc1(x)))
             x = F.relu(self.fc2(x))
-            out = F.sigmoid(self.fc3(x))
+            out = F.softmax(self.fc3(x))
             return out
     
     
     device = "cuda" if torch.cuda.is_available() else "cpu" # Configure device
     model = Network().to(device)
-    summary(model,(3,128,128))
+    summary(model,(3,748,512))
     criterion = nn.CrossEntropyLoss() # Specify the loss layer
-    optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
+    optimizer = optim.SGD(model.parameters(),lr=1e-3, weight_decay=1e-4) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
     
     
     
@@ -125,5 +143,7 @@ for cv in range(6):
     result = evaluate(model, validation_generator)
     cv_results.append(result)
 
+summary(model,(3,128,128))
 np.mean(cv_results)
 result = evaluate(model, training_generator)
+
